a8d3c096f4d859ca6c2f00b7b2e5dcbe085a336f
Sunny Heo <mobileheo@gmail.com>
2018-06-19 22:13:41 -0700

    Basic scss setup without using ejct (#4)
    * Create components and styles directory with App.scss and index.scss
    
    * Add sass script to watch changes in scss file
    
    * Create css files with 'yarn run sass'
    
    * Configure scss with react app
    
    * Update script build command to aware scss for production/deployment
    
    * Update .gitignore & Link daemonite-material scss
    
    * Remove content of README.md
    
    * Remove package.lock.json
    
    * Update yarn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
 
 class App extends Component {
   render() {
@@ -13,6 +12,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button className="btn btn-primary">hello </button>
       </div>
     );
   }
