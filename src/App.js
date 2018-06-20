cacdeeb86feac8add120725767ab08d69f0be178
Sunny Heo <mobileheo@gmail.com>
2018-06-19 21:27:43 -0700

    Update .gitignore & Link daemonite-material scss

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button className="btn btn-primary">hello </button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import daemonite from "daemonite-material";
+// /Users/mobileheo/shall_eat/react-shalleat/node_modules/daemonite-material/css
+
 import "./styles/css/index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+// console.log(daemonite);
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
