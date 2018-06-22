86e700508097fa46f233342a3261f95bf6d8a748
Sunny Heo <mobileheo@gmail.com>
2018-06-21 22:06:59 -0700

    Add CircularProgress component & Implement Google Map on main page

---
+++ src/components/common/CircularProgress.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const CircularProgress = () => {
+  return (
+    <div className="progress-circular">
+      <div className="progress-circular-wrapper">
+        <div className="progress-circular-inner">
+          <div className="progress-circular-left">
+            <div className="progress-circular-spinner" />
+          </div>
+          <div className="progress-circular-gap" />
+          <div className="progress-circular-right">
+            <div className="progress-circular-spinner" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CircularProgress;
--- src/components/pages/MapPage.js
+++ src/components/pages/MapPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 import { googleMapAPI } from "../../requests/configuration";
+import CircularProgress from "../common/CircularProgress";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -8,37 +9,88 @@ class MapPage extends Component {
   constructor(props) {
     super(props);
 
-    // this.state = {
-    //   defualtCenter: this.props.center,
-    //   center: this.state.center
-    // };
+    this.state = {
+      loading: true,
+      center: null,
+      zoom: 11
+    };
   }
 
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.geoSuccess, this.geoError);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  geoError() {
+    alert("Geocoder failed.");
+  }
+
+  geoSuccess = position => {
+    const { latitude: lat, longitude: lng } = position.coords;
+    const center = { lat, lng };
+    const loading = false;
+    this.setState({
+      loading,
+      center
+    });
   };
 
+  componentDidMount() {
+    this.getLocation();
+  }
+
   render() {
-    return (
-      <div className="MapPage" style={{ height: "50vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: googleMapAPI }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={"Kreyser Avrora"}
-          />
-        </GoogleMapReact>
-      </div>
-    );
+    const { loading, center, zoom } = this.state;
+    if (loading) {
+      return <CircularProgress />;
+    } else {
+      return (
+        <div className="MapPage" style={{ height: "100vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: googleMapAPI }}
+            defaultCenter={center}
+            defaultZoom={zoom}
+          >
+            <AnyReactComponent
+              lat={center.lat}
+              lng={center.lng}
+              text={"CurrentLocation"}
+            />
+          </GoogleMapReact>
+        </div>
+      );
+    }
+
+    // return !this.props.isGeolocationAvailable ? (
+    //   <div>Your browser does not support Geolocation</div>
+    // ) : !this.props.isGeolocationEnabled ? (
+    //   <div>Geolocation is not enabled</div>
+    // ) : this.props.coords ? (
+    //   <table>
+    //     <tbody>
+    //       <tr>
+    //         <td>latitude</td>
+    //         <td>{this.props.coords.latitude}</td>
+    //       </tr>
+    //       <tr>
+    //         <td>longitude</td>
+    //         <td>{this.props.coords.longitude}</td>
+    //       </tr>
+    //     </tbody>
+    //   </table>
+    // ) : (
+    //   <div>Getting the location data&hellip; </div>
+    // );
   }
 }
 
 export default MapPage;
+// export default geolocated({
+//   positionOptions: {
+//     enableHighAccuracy: true
+//   },
+//   userDecisionTimeout: null
+// })(MapPage);
