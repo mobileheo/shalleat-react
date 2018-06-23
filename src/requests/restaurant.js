24204689be427ec1997a177c097b714deeb00917
Sunny Heo <mobileheo@gmail.com>
2018-06-22 18:32:03 -0700

    Fix typo on requests/restauratns.js

--- src/requests/restaurant.js
+++ src/requests/restaurant.js
@@ -1,13 +1,13 @@
 import { BASE_URL } from "./fetchConfig";
 import { postData } from "./fetch";
 
-const url = `${BASE_URL}/restaurnats`;
+const url = `${BASE_URL}/restaurants`;
 
 export default {
   async findNearby(filters) {
     try {
-      const restaurnats = await postData(`${url}/all`, filters);
-      return restaurnats;
+      const restaurants = await postData(`${url}/all`, filters);
+      return restaurants;
     } catch (error) {
       throw new Error(error);
     }
