54a109e8c03b01214b4dbfb03bc16d9467ccb25b
Sunny Heo <mobileheo@gmail.com>
2018-06-24 17:48:27 -0700

    Move fetch.js to helper dir  with different file name 'fetchHelper'

--- src/requests/restaurant.js
+++ src/requests/restaurant.js
@@ -1,5 +1,5 @@
 import { BASE_URL } from "./fetchConfig";
-import { postData } from "./fetch";
+import { postData } from "../helper/fetchHelper";
 
 const url = `${BASE_URL}/restaurants`;
 
--- src/requests/user.js
+++ src/requests/user.js
@@ -1,5 +1,5 @@
 import { BASE_URL } from "./fetchConfig";
-import { postData } from "./fetch";
+import { postData } from "../helper/fetchHelper";
 
 export default {
   async signIn(user) {
